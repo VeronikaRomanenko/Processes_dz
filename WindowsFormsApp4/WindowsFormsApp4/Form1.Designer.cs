c5c1ca6fd396700e79281ca48ec9efe6e2f87a80
VeronikaRomanenko <Veronika.Romanenko333@gmail.com>
2019-10-22 22:00:53 +0300

    Commit1

---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Процессы";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(13, 84);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(511, 235);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(141, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 66);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Запустить процесс";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(268, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(128, 66);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Начать обновление";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 326);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Потоки";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(371, 326);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Модули";
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(13, 342);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(256, 201);
+            this.listView2.TabIndex = 9;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView3
+            // 
+            this.listView3.Location = new System.Drawing.Point(268, 342);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(256, 201);
+            this.listView3.TabIndex = 10;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(396, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(128, 66);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Сохранить в файл";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 555);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listView3);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.Columns.Add("ProcessName");
+            listView1.Columns.Add("Id");
+            listView1.Columns.Add("ThreadsCount");
+            listView1.Columns.Add("HandleCount");
+            listView1.View = View.Details;
+            listView2.Columns.Add("Id");
+            listView2.Columns.Add("PriorityLevel");
+            listView2.Columns.Add("StartTime");
+            listView2.View = View.Details;
+            listView3.Columns.Add("ModuleName");
+            listView3.Columns.Add("FileName");
+            listView3.View = View.Details;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            foreach (Process proc in Process.GetProcesses())
+            {
+                ListViewItem item = new ListViewItem(proc.ProcessName);
+                item.SubItems.Add(proc.Id.ToString());
+                item.SubItems.Add(proc.Threads.Count.ToString());
+                item.SubItems.Add(proc.HandleCount.ToString());
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = "calc.exe";
+            proc.Start();
+            MessageBox.Show($"Запущен процесс {proc.ProcessName}");
+            MessageBox.Show($"Процесс завершился с кодом {proc.ExitCode}");
+            MessageBox.Show($"Текущий процесс имеет имя {Process.GetCurrentProcess().ProcessName}");
+        }
+
+        private void listView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            listView2.Items.Clear();
+            listView3.Items.Clear();
+            Process proc = Process.GetProcessById(int.Parse(listView1.SelectedItems[0].SubItems[1].Text));
+            try
+            {
+                foreach (ProcessThread thread in proc.Threads)
+                {
+                    ListViewItem item = new ListViewItem(thread.Id.ToString());
+                    item.SubItems.Add(thread.PriorityLevel.ToString());
+                    item.SubItems.Add(thread.StartTime.ToString());
+                    listView2.Items.Add(item);
+                }
+                foreach (ProcessModule modul in proc.Modules)
+                {
+                    ListViewItem item = new ListViewItem(modul.ModuleName);
+                    item.SubItems.Add(modul.FileName);
+                    listView3.Items.Add(item);
+                }
+            }
+            catch { }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            button1_Click(button1, new EventArgs());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                using (XmlTextWriter wr = new XmlTextWriter(dialog.FileName, Encoding.Unicode))
+                {
+                    wr.Formatting = Formatting.Indented;
+                    wr.WriteStartDocument();
+                    wr.WriteStartElement("Processes");
+                    foreach (ListViewItem proc in listView1.Items)
+                    {
+                        wr.WriteStartElement("Process");
+                        wr.WriteElementString("ProcessName", proc.SubItems[0].Text.ToString());
+                        wr.WriteElementString("Id", proc.SubItems[1].Text.ToString());
+                        wr.WriteElementString("ThreadsCount", proc.SubItems[2].Text.ToString());
+                        wr.WriteElementString("HandleCount", proc.SubItems[3].Text.ToString());
+                        wr.WriteEndElement();
+                    }
+                    wr.WriteEndElement();
+                    wr.WriteEndDocument();
+                }
+            }
+        }
+    }
+}
